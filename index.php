ebc8a807a2e7c90ce2e2b8523150bb2dfb35e91c
ignaszienius <30698624+ignaszienius@users.noreply.github.com>
2017-09-19 16:23:58 +0300

    padariau kad nuskaitytu duombaze

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+	
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "products";
+$err = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+mysqli_set_charset($conn, "utf8");
+//echo "Connected successfully<br >";
+$sql = "SELECT * FROM products ORDER BY id";
+$result = mysqli_query($conn, $sql);
+$products = [];
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+    	array_push($products, $row);
+    }
+} else {
+    $err = "Prekiu nerasta";
+}
+mysqli_close($conn);
+include "view.php";
---
+++ view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<table class="table">
+			  <thead>
+			    <tr>
+			      <th>#</th>
+			      <th>Pavadinimas</th>
+			      <th>Kaina</th>
+			      <th>Kiekis</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+				<?php
+					foreach ($products as $product) {
+						echo "<tr><td>". $product['id'] ."</td><td>". $product['pavadinimas'] . "</td><td class='price'>". $product['kaina'] . "</td><td>". $product['kiekis'] . "</td></tr>";
+					}
+				?>
+			  </tbody>
+			</table>
+		</div>
+	</div>
+	<script>
+		$prices = document.getElementsByClassName('price');
+		for (var i = 0; i <= $prices.length - 1; i++) {
+			console.log($prices[i]);
+		}
+	</script>
+</body>
+</html>
